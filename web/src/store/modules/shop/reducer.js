556c19cf231316e0a8081b20e2dc3287da76d209
Pedro Giampietro <pedrogiampietro@hotmail.com>
2021-01-20 19:08:44 -0300

    Configure Redux Saga

--- web/src/store/modules/shop/reducer.js
+++ web/src/store/modules/shop/reducer.js
@@ -2,20 +2,27 @@ import produce from 'immer'
 import types from './types'
 
 const INITIAL_STATE = {
-	customer: {},
+  customer: {},
+  petshops: {},
 }
 
 function shop(state = INITIAL_STATE, action) {
-	switch (action.type) {
-		case types.SET_CUSTOMER: {
-			return produce(state, (draft) => {
-				draft.customer = action.customer
-			})
-		}
+  switch (action.type) {
+    case types.SET_CUSTOMER: {
+      return produce(state, (draft) => {
+        draft.customer = action.customer
+      })
+    }
 
-		default:
-			return state
-	}
+    case types.SET_PETSHOPS: {
+      return produce(state, (draft) => {
+        draft.petshops = action.petshops
+      })
+    }
+
+    default:
+      return state
+  }
 }
 
 export default shop
---
+++ web/src/store/modules/shop/sagas.js
@@ -0,0 +1,12 @@
+import { takeLatest, all, call, put } from 'redux-saga/effects'
+import types from './types'
+import api from '../../../services/api'
+import { setPetshops } from './actions'
+
+export function* requestPetshops() {
+  const response = yield call(api.get, '/petshops')
+  const res = response.data
+  yield put(setPetshops(res.petshops))
+}
+
+export default all([takeLatest(types.REQUEST_PETSHOPS, requestPetshops)])
